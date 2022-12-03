a5fd43d50b4bc88389c0a80f5bac6a7297451796
LiJnD <luico199@gmal.com>
2022-12-03 00:28:22 -0500

    four  commit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,37 +1,50 @@
 import React from 'react'
 import { useState } from 'react'
+import { db } from '../firebase'
+import { collection, doc, addDoc } from 'firebase/firestore'
 
 const Formulario = () => {
 
-const [Ropa, setRopa] = useState('')
-const [Descripcion, setDescripcion] = useState('')
-const [ListaRopa, setListaRopa] = useState([])
+    const [Ropa, setRopa] = useState('')
+    const [Descripcion, setDescripcion] = useState('')
+    const [ListaRopa, setListaRopa] = useState([])
 
+    const GuardarRopa = async (e) => {
+        e.preventDefault()
+        try {
+            const data = await addDoc(collection(db, 'Ropa'), {
+                nombreRopa: Ropa,
+                nombreDescripcion: Descripcion
+            })
+        }catch(error){
+            console.log(error)
+        }
+}
 
-  return (
-    <div className='container mt-5'>
-      <h1 className="text-cent">BesrkhaRoot</h1>
-      <hr />
-      <div className='row'>
-        <div className='col-8'>
-            <h4 className="text-center">Listado de Ropas</h4>
-            <ul className='list-group'>
-            <li className='list-group-item'>Ropa One</li>
-            <li className='list-group-item'>Ropa Two</li>
-            </ul>
+    return (
+        <div className='container mt-5'>
+            <h1 className="text-cent">BesrkhaRoot</h1>
+            <hr />
+            <div className='row'>
+                <div className='col-8'>
+                    <h4 className="text-center">Listado de Ropas</h4>
+                    <ul className='list-group'>
+                        <li className='list-group-item'>Ropa One</li>
+                        <li className='list-group-item'>Ropa Two</li>
+                    </ul>
+                </div>
+                <div className='col-4'>
+                    <h4 className='text-center'>Agregar Ropa</h4>
+                    <form action="">
+                        <input type="text" className='form-control mb-2' placeholder='Ingrese Ropa' value={Ropa} />
+                        <input type="text" className='form-control mb-2' placeholder='Ingrese Descripción' value={Descripcion}/>
+                        <button className="btn btn-primary btn-block" on="submit">Agregar</button>
+                        <button className="btn btn-dark btn-block mx-2">Cancelar</button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className='col-4'>
-            <h4 className='text-center'>Agregar Ropa</h4>
-            <form action="">
-                <input type="text" className='form-control mb-2' placeholder='Ingrese Ropa' />
-                <input type="text" className='form-control mb-2' placeholder='Ingrese Descripción' />
-                <button className="btn btn-primary btn-block" on = "submit">Agregar</button>
-                <button className="btn btn-dark btn-block mx-2">Cancelar</button>
-            </form>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Formulario
