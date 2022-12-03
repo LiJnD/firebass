a6dadb3dcdc4549885c19719487e09dc3531cf5a
LiJnD <luico199@gmal.com>
2022-12-03 15:31:15 -0500

    eithg commit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { db } from '../firebase'
-import { collection, doc, addDoc } from 'firebase/firestore'
-import { async } from '@firebase/util'
+import { collection, doc, addDoc, onSnapshot, deleteDoc } from 'firebase/firestore'
 
 const Formulario = () => {
 
+    //Campos
+    const [id, setId] = useState(0)
     const [Ropa, setRopa] = useState('')
     const [Descripcion, setDescripcion] = useState('')
     const [ListaRopa, setListaRopa] = useState([])
+    const [modoEdition, setModoEdition] = useState(false)
 
-useEffect(()=>{
-    const obtenerDatos = async () =>{
-        try {
-            
-        } catch (error) {
-            console.log(error)
+    useEffect(() => {
+        const obtenerDatos = async () => {
+            try {
+                await onSnapshot(collection(db, 'Ropa'), (query) => {
+                    setListaRopa(query.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+                })
+            } catch (error) {
+                console.log(error)
+            }
         }
-    } 
-})
-
+        obtenerDatos(ListaRopa);
+    }, [])
+    //guardado
     const GuardarRopa = async (e) => {
         e.preventDefault()
         try {
@@ -28,7 +33,7 @@ useEffect(()=>{
                 nombreDescripcion: Descripcion
             })
             setListaRopa(
-                [...ListaRopa, { nombreRopa: Ropa,nombreDescripcion: Descripcion, id: data.id }]
+                [...ListaRopa, { nombreRopa: Ropa, nombreDescripcion: Descripcion, id: data.id }]
             )
             setRopa('')
             setDescripcion('')
@@ -37,7 +42,25 @@ useEffect(()=>{
             console.log(error)
         }
     }
+    //Editar item
+    const EditarRopa = item => {
+        setId(item.id)
+        setRopa(item.nombreRopa)
+        setDescripcion(item.nombreDescripcion)
+        setModoEdition(true)
+    }
 
+    //eliminar items
+    const EliminarRopa = async id => {
+        try {
+            await deleteDoc(doc(db, 'Ropa', id))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    //correr programas
     return (
         <div className='container mt-5'>
             <h1 className="text-cent">BesrkhaRoot</h1>
@@ -46,17 +69,34 @@ useEffect(()=>{
                 <div className='col-8'>
                     <h4 className="text-center">Listado de Ropas</h4>
                     <ul className='list-group'>
-                        <li className='list-group-item'>Ropa One</li>
-                        <li className='list-group-item'>Ropa Two</li>
+                        {
+                            ListaRopa.map(item => (
+                                <li className='list-group-item' key={item.id}>
+                                    <span className="lead">{item.nombreRopa}-{item.nombreDescripcion}</span>
+                                    <button className="btn btn-warning btn-sm fload-end" onClick={() => EditarRopa(item)}>Editar</button>
+                                    <button className="btn btn-danger btn-sm fload-end mx-2" onClick={() => EliminarRopa(item.id)}>Eliminar</button>
+
+                                </li>
+                            ))
+                        }
                     </ul>
                 </div>
                 <div className='col-4'>
-                    <h4 className='text-center'>Agregar Ropa</h4>
+                    <h4 className='text-center'>{modoEdition ? 'Editando Ropa' : 'Agrege Ropa'}</h4>
                     <form onSubmit={GuardarRopa}>
                         <input type="text" className='form-control mb-2' placeholder='Ingrese Ropa' value={Ropa} onChange={(e) => setRopa(e.target.value)} />
                         <input type="text" className='form-control mb-2' placeholder='Ingrese Descripción' value={Descripcion} onChange={(e) => setDescripcion(e.target.value)} />
-                        <button className="btn btn-primary btn-block" on="submit">Agregar</button>
-                        <button className="btn btn-dark btn-block mx-2">Cancelar</button>
+                        {
+                            //botones de agregar
+                            modoEdition ? (
+                                <>
+                                    <button className="btn btn-warning btn-block" on="submit">Editar</button>
+                                    <button className="btn btn-dark btn-block mx-2">Cancelar</button>
+                                </>
+                            )
+                            :
+                            <button className="btn btn-primary btn-block" on="submit">Agregar</button>
+                        }
                     </form>
                 </div>
             </div>
